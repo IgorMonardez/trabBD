506c729d557586646ee3775e4b4b5b7a3d8cf6dd
Antonio Romano <antonio.romano@quantumfinance.com.br>
2024-07-01 10:25:37 -0300

    criação trigger de refresh para view materializada top_canais

---
+++ trigger-rascunho/triggers_para_view_materializada_top_canais.sql
@@ -0,0 +1,100 @@
+-- Função para atualizar a view materializada top_canais
+
+CREATE OR REPLACE FUNCTION refresh_top_canais()
+RETURNS TRIGGER AS $$
+BEGIN
+    REFRESH MATERIALIZED VIEW top_canais;
+    RETURN NULL;
+END;
+$$ LANGUAGE plpgsql;
+
+
+-- Insert Patrocinio
+CREATE TRIGGER trigger_refresh_top_canais_insert_patrocinio
+AFTER INSERT ON trabbd2.patrocinio
+FOR EACH ROW
+EXECUTE FUNCTION refresh_top_canais();
+
+-- UPDATE patrocinio
+CREATE TRIGGER trigger_refresh_top_canais_update_patrocinio
+AFTER UPDATE ON trabbd2.patrocinio
+FOR EACH ROW
+EXECUTE FUNCTION refresh_top_canais();
+
+-- DELETE patrocinio
+CREATE TRIGGER trigger_refresh_top_canais_delete_patrocinio
+AFTER DELETE ON trabbd2.patrocinio
+FOR EACH ROW
+EXECUTE FUNCTION refresh_top_canais();
+
+-- INSERT inscricao
+CREATE TRIGGER trigger_refresh_top_canais_insert_inscricao
+AFTER INSERT ON trabbd2.inscricao
+FOR EACH ROW
+EXECUTE FUNCTION refresh_top_canais();
+
+-- UPDATE inscricao
+CREATE TRIGGER trigger_refresh_top_canais_update_inscricao
+AFTER UPDATE ON trabbd2.inscricao
+FOR EACH ROW
+EXECUTE FUNCTION refresh_top_canais();
+
+-- DELETE inscricao
+CREATE TRIGGER trigger_refresh_top_canais_delete_inscricao
+AFTER DELETE ON trabbd2.inscricao
+FOR EACH ROW
+EXECUTE FUNCTION refresh_top_canais();
+
+-- INSERT nivelcanal
+CREATE TRIGGER trigger_refresh_top_canais_insert_nivelcanal
+AFTER INSERT ON trabbd2.nivelcanal
+FOR EACH ROW
+EXECUTE FUNCTION refresh_top_canais();
+
+-- UPDATE nivelcanal
+CREATE TRIGGER trigger_refresh_top_canais_update_nivelcanal
+AFTER UPDATE ON trabbd2.nivelcanal
+FOR EACH ROW
+EXECUTE FUNCTION refresh_top_canais();
+
+-- DELETE  nivelcanal
+CREATE TRIGGER trigger_refresh_top_canais_delete_nivelcanal
+AFTER DELETE ON trabbd2.nivelcanal
+FOR EACH ROW
+EXECUTE FUNCTION refresh_top_canais();
+
+-- INSERT  doacao
+CREATE TRIGGER trigger_refresh_top_canais_insert_doacao
+AFTER INSERT ON trabbd2.doacao
+FOR EACH ROW
+EXECUTE FUNCTION refresh_top_canais();
+
+-- UPDATE doacao
+CREATE TRIGGER trigger_refresh_top_canais_update_doacao
+AFTER UPDATE ON trabbd2.doacao
+FOR EACH ROW
+EXECUTE FUNCTION refresh_top_canais();
+
+-- DELETE doacao
+CREATE TRIGGER trigger_refresh_top_canais_delete_doacao
+AFTER DELETE ON trabbd2.doacao
+FOR EACH ROW
+EXECUTE FUNCTION refresh_top_canais();
+
+-- INSERT video
+CREATE TRIGGER trigger_refresh_top_canais_insert_video
+AFTER INSERT ON trabbd2.video
+FOR EACH ROW
+EXECUTE FUNCTION refresh_top_canais();
+
+-- UPDATE video
+CREATE TRIGGER trigger_refresh_top_canais_update_video
+AFTER UPDATE ON trabbd2.video
+FOR EACH ROW
+EXECUTE FUNCTION refresh_top_canais();
+
+-- DELETE video
+CREATE TRIGGER trigger_refresh_top_canais_delete_video
+AFTER DELETE ON trabbd2.video
+FOR EACH ROW
+EXECUTE FUNCTION refresh_top_canais();
