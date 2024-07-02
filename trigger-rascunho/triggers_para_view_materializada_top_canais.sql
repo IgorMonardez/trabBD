80bf087c6988f11c52d2bbd5b54cd35d7b851695
caiovelp <caio.paranhos@traivefinance.com>
2024-07-02 00:37:44 -0300

    Triggers para a view materializada

--- trigger-rascunho/triggers_para_view_materializada_top_canais.sql
+++ trigger-rascunho/triggers_para_view_materializada_top_canais.sql
@@ -1,100 +1,18 @@
--- Função para atualizar a view materializada top_canais
-
 CREATE OR REPLACE FUNCTION refresh_top_canais()
 RETURNS TRIGGER AS $$
 BEGIN
     REFRESH MATERIALIZED VIEW top_canais;
     RETURN NULL;
-END;
-$$ LANGUAGE plpgsql;
+END $$ LANGUAGE plpgsql;
 
+CREATE TRIGGER refresh_top_canais_after_insert_or_update_or_delete
+AFTER INSERT OR UPDATE OR DELETE ON Patrocinio
+FOR EACH ROW EXECUTE FUNCTION refresh_top_canais();
 
--- Insert Patrocinio
-CREATE OR REPLACE TRIGGER trigger_refresh_top_canais_insert_patrocinio
-AFTER INSERT ON trabbd2.patrocinio
-FOR EACH ROW
-EXECUTE FUNCTION refresh_top_canais();
+CREATE TRIGGER refresh_top_canais_after_insert_or_update_or_delete
+AFTER INSERT OR UPDATE OR DELETE ON Inscricao
+FOR EACH ROW EXECUTE FUNCTION refresh_top_canais();
 
--- UPDATE patrocinio
-CREATE OR REPLACE TRIGGER trigger_refresh_top_canais_update_patrocinio
-AFTER UPDATE ON trabbd2.patrocinio
-FOR EACH ROW
-EXECUTE FUNCTION refresh_top_canais();
-
--- DELETE patrocinio
-CREATE OR REPLACE  TRIGGER trigger_refresh_top_canais_delete_patrocinio
-AFTER DELETE ON trabbd2.patrocinio
-FOR EACH ROW
-EXECUTE FUNCTION refresh_top_canais();
-
--- INSERT inscricao
-CREATE OR REPLACE TRIGGER trigger_refresh_top_canais_insert_inscricao
-AFTER INSERT ON trabbd2.inscricao
-FOR EACH ROW
-EXECUTE FUNCTION refresh_top_canais();
-
--- UPDATE inscricao
-CREATE OR REPLACE TRIGGER trigger_refresh_top_canais_update_inscricao
-AFTER UPDATE ON trabbd2.inscricao
-FOR EACH ROW
-EXECUTE FUNCTION refresh_top_canais();
-
--- DELETE inscricao
-CREATE OR REPLACE TRIGGER trigger_refresh_top_canais_delete_inscricao
-AFTER DELETE ON trabbd2.inscricao
-FOR EACH ROW
-EXECUTE FUNCTION refresh_top_canais();
-
--- INSERT nivelcanal
-CREATE OR REPLACE TRIGGER trigger_refresh_top_canais_insert_nivelcanal
-AFTER INSERT ON trabbd2.nivelcanal
-FOR EACH ROW
-EXECUTE FUNCTION refresh_top_canais();
-
--- UPDATE nivelcanal
-CREATE OR REPLACE TRIGGER trigger_refresh_top_canais_update_nivelcanal
-AFTER UPDATE ON trabbd2.nivelcanal
-FOR EACH ROW
-EXECUTE FUNCTION refresh_top_canais();
-
--- DELETE  nivelcanal
-CREATE OR REPLACE TRIGGER trigger_refresh_top_canais_delete_nivelcanal
-AFTER DELETE ON trabbd2.nivelcanal
-FOR EACH ROW
-EXECUTE FUNCTION refresh_top_canais();
-
--- INSERT  doacao
-CREATE OR REPLACE TRIGGER trigger_refresh_top_canais_insert_doacao
-AFTER INSERT ON trabbd2.doacao
-FOR EACH ROW
-EXECUTE FUNCTION refresh_top_canais();
-
--- UPDATE doacao
-CREATE OR REPLACE TRIGGER trigger_refresh_top_canais_update_doacao
-AFTER UPDATE ON trabbd2.doacao
-FOR EACH ROW
-EXECUTE FUNCTION refresh_top_canais();
-
--- DELETE doacao
-CREATE OR REPLACE TRIGGER trigger_refresh_top_canais_delete_doacao
-AFTER DELETE ON trabbd2.doacao
-FOR EACH ROW
-EXECUTE FUNCTION refresh_top_canais();
-
--- INSERT video
-CREATE OR REPLACE TRIGGER trigger_refresh_top_canais_insert_video
-AFTER INSERT ON trabbd2.video
-FOR EACH ROW
-EXECUTE FUNCTION refresh_top_canais();
-
--- UPDATE video
-CREATE OR REPLACE TRIGGER trigger_refresh_top_canais_update_video
-AFTER UPDATE ON trabbd2.video
-FOR EACH ROW
-EXECUTE FUNCTION refresh_top_canais();
-
--- DELETE video
-CREATE OR REPLACE TRIGGER trigger_refresh_top_canais_delete_video
-AFTER DELETE ON trabbd2.video
-FOR EACH ROW
-EXECUTE FUNCTION refresh_top_canais();
+CREATE TRIGGER refresh_top_canais_after_insert_or_update_or_delete
+AFTER INSERT OR UPDATE OR DELETE ON Doacao
+FOR EACH ROW EXECUTE FUNCTION refresh_top_canais();
